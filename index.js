4cac1df05198c0ecd39d6d96203d12be09910e46
erasedbeyond <shivendra22031996@gmail.com>
2020-11-22 11:09:00 +0530

    setup static file access for layouts and pages

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const app= express();
-const port = 8000;
+const port = 8001;
 
-app.use('/', require('./routes'))
+
+
+app.use(expressEjsLayouts);
+
+
+//setting up static files
+app.use(express.static('assets'));
+
+
+//use  express route
+app.use('/', require('./routes'));
+
+
+//set up view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error is running the server: ${err}`)

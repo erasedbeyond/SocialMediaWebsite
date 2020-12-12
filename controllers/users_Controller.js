e0a4ee3bb95a3bed7afd821bd513d3a084bb2d96
erasedbeyond <shivendra22031996@gmail.com>
2020-12-12 18:12:44 +0530

    Set Authentication using Passport

--- controllers/users_Controller.js
+++
@@ -1,7 +0,0 @@
-module.exports.profile = function(req,res){
-    return res.end('<h1>Profile page<h1>')
-}
-
-module.exports.post = function(req,res){
-    return res.end('<h1>Post page<h1>');
-}
--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 const express = require('express');
-const expressEjsLayouts = require('express-ejs-layouts');
-const app= express();
-const port = 8001;
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 
-app.use(expressEjsLayouts);
 
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-//setting up static files
-app.use(express.static('assets'));
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-//use  express route
+app.use(passport.setAuthenticatedUser);
+
+// use express router
 app.use('/', require('./routes'));
 
 
-//set up view engine
-app.set('view engine', 'ejs');
-app.set('views','./views');
-
-
-
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error is running the server: ${err}`)
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log(`server is up and running at port: ${port}`)
+
+    console.log(`Server is running on port: ${port}`);
 });
